df3c021617c3180935d0ecbad92e2c9a8f93c562
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-12-18 19:01:58 +0300

    feat: added weaatherjs

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,21 @@
-import React from "react";
-
 function Weather() {
-  return <div>Weather</div>;
+  return (
+    <div className="container">
+      <div className="location_name">
+        <p>Trabzon</p>
+      </div>
+      <div className="coordinations">
+        <p>39.8333</p>
+        <p>40.9167</p>
+      </div>
+      <div className="temp">
+        <p>12&deg;C</p>
+      </div>
+      <div className="weather">
+        <p>Sun</p>
+      </div>
+    </div>
+  );
 }
 
 export default Weather;
