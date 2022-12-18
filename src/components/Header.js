3ca1fdb9c4cc38428c01440ea2aff65eaaf445bc
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-12-18 17:52:06 +0300

    style: edited header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 function Header({ cities, setCities }) {
   return (
-    <header className="w-full flex flex-col justify-center items-center">
-      <h1>Weather App</h1>
+    <header className="w-full flex flex-col justify-center items-center text-white">
+      <h1 className="text-5xl tracking-wider my-2 ">Weather App</h1>
       <input
         type="text"
         value={cities}
         onChange={(e) => setCities(e.target.value)}
+        placeholder="Enter Your Location"
+        className="w-72 p-1 my-2 bg-[#263159] rounded-xl cursor-pointer placeholder:text-gray-400 hover:bg-[#212b4c] focus:outline-none"
       />
-      <button>Get Result</button>
+      <button
+        type="button"
+        class="text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg w-32 text-sm p-2 m-2"
+      >
+        Get Result
+      </button>
     </header>
   );
 }
