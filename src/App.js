fce34ada01a930d5c09a9c614a32daeebb798ba0
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-12-18 17:14:49 +0300

    feat: imported components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Weather from "./components/Weather";
+import WeatherDetails from "./components/WeatherDetails";
 
 function App() {
+  const [cities, setCities] = useState("");
+  const [weatherData, setWeatherData] = useState({});
+
   return (
-    <div className="App">
-      <Header />
+    <div className="App w-full h-screen m-0 p-0 box-border">
+      <Header cities={cities} setCities={setCities} />
     </div>
   );
 }
