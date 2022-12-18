ab6bbb9669eb66e70520c4b0fb6467ff5b02b84b
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-12-18 17:21:30 +0300

    feat: created input and button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
-import React from "react";
-
-function Header() {
-  return <div>Header</div>;
+function Header({ cities, setCities }) {
+  return (
+    <header className="w-full flex flex-col justify-center items-center">
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        value={cities}
+        onChange={(e) => setCities(e.target.value)}
+      />
+      <button>Get Result</button>
+    </header>
+  );
 }
 
 export default Header;
